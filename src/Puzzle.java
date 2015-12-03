23cf157c8bd3928d6ceb28fcce9c8444a4add167
Ants Kristjan Rooma <antskristjan.rooma@itcollege.ee>
2015-12-03 21:50:16 +0200

    no message

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -9,99 +9,63 @@ public class Puzzle {
     */
    public static void main (String[] args) {
 	   String[] test = {"A", "B", "C"};
-	   
-	   validate(test);
-	   
+//	   String s = "CBCHDSF";
+//	   validate(test);
+
 //	   System.out.println(getUniqueLetters (test));
-	   setExceptions(test);
+//	   setExceptions(test);
 //	   System.out.println(cannotBeZero);
 	   
-	   System.out.println(!cannotBeZero.contains(String.valueOf("1")));
+//	   System.out.println(!cannotBeZero.contains(String.valueOf(s.charAt(0))));
 	   
 //	   ArrayList<String> permutations = getPermutations (getUniqueLetters (test));
+	   String s = "0123456ABC";
+	   String a = "ABC";
 	   
-//	   System.out.println(permutations.size());
-   }
-   
+	   for (int i = 0; i < test.length; i++) {
 
-   
-   public static ArrayList<String> getPermutations (String input) {
-	   ArrayList<String> permutations = new ArrayList<String>();
-	   fillPermutationArray ("", input, permutations);
-	   
-	   return permutations;
-   }
-   
-   private static void fillPermutationArray (String prefix, String input, ArrayList<String> permutations) {
-	   int l = input.length();
-	   
-	   if (l == 0) {
-		   if (!cannotBeZero.contains(String.valueOf(prefix.charAt(0)))) {
-			   permutations.add(prefix);
-		   }
-	   } else {
-		   for (int i = 0; i < l; i++) {
-			   fillPermutationArray (prefix + input.charAt(i), input.substring(0, i) + input.substring(i+1, l), permutations);
-		   }
-	   }
-   }
-   
-   /**
-    * Puts the letters that cannot have the value of 0 in a global string.
-    * These letters are the first ones of the puzzle arguments.
-    * @param Array containing strings.
-    */
-   public static void setExceptions(String[] input) {
-	   StringBuffer sb = new StringBuffer();
-	   
-	   for (String s : input) {
-		   sb.append(s.charAt(0));
 	   }
 	   
-	   cannotBeZero = sb.toString();
+	   char[] array = a.toCharArray();
+	   
+	   for (int i = 0; i < array.length; i++) {
+		   array[i] = (char) (s.indexOf(array[i]) + '0');
+	   }
+	   a = String.valueOf(array);
+	   
+	   System.out.println(a);
    }
    
-   /**
-    * @param Puzzle argument array.
-    * @return String containing unique letters and filler.
-    */
-   public static String getUniqueLetters (String[] input) {
-	   StringBuffer sb = new StringBuffer();
-	   int counter = 0;
+   public static void solvePuzzle (String[] input) {
+	   validate (input);
+	   setExceptions (input);
 	   
-	   // adds all the unique letters into the stringbuffer.
-	   for (String s : input) {
-		   for (int i = 0; i < s.length(); i++) {
-			   if (sb.indexOf(String.valueOf(s.charAt(i))) < 0) {
-				   sb.append(s.charAt(i));
+	   String[] replacements = {};
+	   ArrayList<String> results = new ArrayList<String>();
+	   
+	   String uniqueLetters = getUniqueLetters (input);
+	   String uniqueLettersWithFiller = getUniquesWithFiller(uniqueLetters);
+	   ArrayList<String> permutations = getPermutations (uniqueLettersWithFiller);
+	   
+	   for (String s : permutations) {
+		   for (int i = 0; i < input.length; i++) {
+			   for (int j = 0; j < input[i].length(); j++) {
+//				    = input[i].replace(input[i].charAt(j), (char) s.indexOf(input[i].charAt(j)));
 			   }
-		   }
+		   }		   
 	   }
-	   
-	   // can't have more than 10 unique letters.
-	   if (sb.length() > 10) {
-		   throw new RuntimeException ("Too many unique letters across all arguments: " + argsToString(input));
-	   }
-	   
-	   // if the unique letter count isn't 10, non-letters will be added.
-	   while (sb.length() < 10) {
-		   sb.append(counter);
-		   counter++;
-	   }
-	   
-	   return sb.toString();
    }
 
    /**
     * Validates the puzzle's input arguments.
     */
-   public static void validate (String[] input) {
+   private static void validate (String[] input) {
 	   // checks if the correct amount of arguments were given.
 	   if (input.length != 3) {
 		   throw new RuntimeException ("Input '" + argsToString (input) + "' doesn't containt the correct amount [3] of words.");
 	   }
 	   
-	   // checks if the strings are only uppercase letters.
+	   // checks if the strings are only upper-case letters.
 	   // makes sure the arguments are no longer than 18 letters.
 	   for (String s : input) {
 		   if (!s.matches("[A-Z]+")) {
@@ -120,10 +84,97 @@ public class Puzzle {
    }
    
    /**
-    * @param input -- An array containing strings. 
+    * Puts the letters that cannot have the value of 0 in a global string.
+    * These letters are the first ones of the puzzle arguments.
+    * @param input Array containing strings.
+    */
+   private static void setExceptions(String[] input) {
+	   StringBuffer sb = new StringBuffer();
+	   
+	   for (String s : input) {
+		   sb.append(s.charAt(0));
+	   }
+	   
+	   cannotBeZero = sb.toString();
+   }
+   
+   /**
+    * @param input Puzzle argument array.
+    * @return String containing unique letters and filler.
+    */
+   private static String getUniqueLetters (String[] input) {
+	   StringBuffer sb = new StringBuffer();
+	   
+	   // adds all the unique letters into the string-buffer.
+	   for (String s : input) {
+		   for (int i = 0; i < s.length(); i++) {
+			   if (sb.indexOf(String.valueOf(s.charAt(i))) < 0) {
+				   sb.append(s.charAt(i));
+			   }
+		   }
+	   }
+	   
+	   // can't have more than 10 unique letters.
+	   if (sb.length() > 10) {
+		   throw new RuntimeException ("Too many unique letters across all arguments: " + argsToString(input));
+	   }
+	   
+	   return sb.toString();
+   }
+   
+   /**
+    * Adds filler elements to the unique letters so permutations can be made.
+    * @param input containing unique letters.
+    * @return String containing unique letters and filler elements.
+    */
+   private static String getUniquesWithFiller(String input) {
+	   int counter = 0;
+	   StringBuffer sb = new StringBuffer();
+	   
+	   sb.append(input);
+
+	   while (sb.length() < 10) {
+		   sb.append(counter);
+		   counter++;
+	   }
+	   
+	   return sb.toString();
+   }
+   
+   /**
+    * @param input String containing unique letters and filler.
+    * @return Array-list containing permutations of the string. A few permutations are omitted.
+    */
+   // http://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+   private static ArrayList<String> getPermutations (String input) {
+	   ArrayList<String> permutations = new ArrayList<String>();
+	   fillPermutationArray ("", input, permutations);
+	   
+	   System.out.println(permutations.size());
+	   return permutations;
+   }
+   
+   private static void fillPermutationArray (String prefix, String input, ArrayList<String> permutations) {
+	   int l = input.length();
+	   
+	   if (l == 0) {
+		   // checks if the permutation doesn't start with the first letter of a puzzle argument.
+		   // if it doesn't, add it to the list.
+		   if (!cannotBeZero.contains(String.valueOf(prefix.charAt(0)))) {
+			   permutations.add(prefix);
+		   }
+	   } else {
+		   for (int i = 0; i < l; i++) {
+			   fillPermutationArray (prefix + input.charAt(i), input.substring(0, i) + input.substring(i+1, l), permutations);
+		   }
+	   }
+   }
+   
+   /**
+    * @param input an array containing strings. 
     * @return A string containing the array's strings' values separated separated by a whitespace.
     */
-   public static String argsToString(String[] input) {
+   private static String argsToString(String[] input) {
 	   StringBuffer sb = new StringBuffer();
 	   
 	   for (String s : input) {
